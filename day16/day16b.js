25fc02657190072975fcd2d1be98770f5e9f6689
Roel <roel@lakenvelt.nl>
2019-12-23 16:58:17 +0100

    Day 16 final

--- day16/day16b.js
+++ day16/day16b.js
@@ -1,3 +1,7 @@
+// Assumtion is that the message offset is more than half of the length op the total input string
+// In that case all of the pattern values before the offset are 0 and all of the pattern values
+// after the offset are 1. So we can sik the calculation of the ppart before the offset and we don't
+// have to determine the pattern value of the rest because it is always 1.
 const shared = require('../shared');
 const BASEPATTERN = [0, 1, 0, -1];
 
@@ -13,11 +17,17 @@ shared.start("day 16B");
 function fft(inputSignal) {
     let outputElements = '';
     let total = 0;
+    let lasttotal = 0;
     for (let element=0; element<inputElementsLength; element++) {
         total = 0;
-        for (let index=element; index <inputElementsLength; index++) {
-            total += inputSignal[index];
+        if (element > 0) {
+            total=lasttotal-inputSignal[element-1];
+        } else {
+            for (let index=element; index <inputElementsLength; index++) {
+                total+=inputSignal[index];
+            }
         }
+        lasttotal = total;
         outputElements = outputElements.concat((total%10).toString());
     }
     return outputElements.split('').map(x => parseInt(x));
