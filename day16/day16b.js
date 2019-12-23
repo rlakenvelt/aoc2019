f1f64d307ff542886a8989c5cd447c4d6d4691eb
Roel <roel@lakenvelt.nl>
2019-12-23 15:16:45 +0100

    Day 16b

--- day16/day16b.js
+++ day16/day16b.js
@@ -5,33 +5,22 @@ const singleinput = shared.getInput()[0];
 const offset = parseInt(singleinput.substring(0,7));
 const input = singleinput.repeat(10000).substring(offset);
 
-const inputElements = input.split('');
+const inputElements = input.split('').map(x => parseInt(x));
 const inputElementsLength = inputElements.length;
 
 shared.start("day 16B");
-//61706040
-function patternForElement(element, index) {
-    return BASEPATTERN[Math.floor(index % (element * 4) / element)];
-}
 
 function fft(inputSignal) {
-    let starttime;
-    let endtime;
-    starttime = new Date();
-    let outputElements = [];
-    for (let i=1; i<=inputElementsLength; i++) {
-        // if (i % 1000 === 0) {
-        //     endtime = new Date();
-        //     console.log(i, endtime - starttime);
-        //     starttime = endtime;
-        // }
-        let total = 0;
-        for (let index=i; index <=inputElementsLength; index++) {
-            total += inputSignal[index-1] * patternForElement(i + offset, index + offset);
+    let outputElements = '';
+    let total = 0;
+    for (let element=0; element<inputElementsLength; element++) {
+        total = 0;
+        for (let index=element; index <inputElementsLength; index++) {
+            total += inputSignal[index];
         }
-        outputElements.push(Math.abs(total) % 10);
+        outputElements = outputElements.concat((total%10).toString());
     }
-    return outputElements;
+    return outputElements.split('').map(x => parseInt(x));
 }
 let inputValue = [...inputElements];
 for (let i=0; i<100; i++) {
