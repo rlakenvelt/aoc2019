c145f4b7f1b57c1bfa72682af313f5dbb7abd011
Roel <roel@lakenvelt.nl>
2020-01-06 21:55:09 +0100

    Day 22 part 2

--- README.md
+++ README.md
@@ -2,7 +2,10 @@
 
 https://adventofcode.com/2019
 
+I used the solution theory for part 2 of day 22 from https://www.reddit.com/r/adventofcode/comments/ee0rqi/2019_day_22_solutions/fbnifwk/
+
 Not solved:
 - Day 18
-- Day 22 part 2
 - Day 25 part 2
+
+
--- day22/day22b.js
+++ day22/day22b.js
@@ -1,3 +1,5 @@
+let bigintCryptoUtils = require('bigint-crypto-utils');
+
 const shared = require('../shared');
 const shuffles = shared.getInput()
                        .reduce((list, line) => {
@@ -12,45 +14,45 @@ const shuffles = shared.getInput()
                                 list.push({type: 2});
                             };
                             return list;
-                       }, []);
-const DECKSIZE=119315717514047;
-const SHUFFLES=101741582076661;
-let lastPosition = 2019;
+                       }, [])
+                       .reverse();
+                       
+const DECKSIZE=BigInt(119315717514047);
+const SHUFFLES=BigInt(101741582076661);
+let SEARCHPOSITION = BigInt(2020);
 
-function dealIntoNewStack () {
-    lastPosition=DECKSIZE-lastPosition-1;
+function reverseDealIntoNewStack (position) {
+    return DECKSIZE-BigInt(position)-BigInt(1);
 }        
 
-function dealWithIncrement (increment) {
-    lastPosition=(lastPosition*increment)%DECKSIZE;
+function reverseDealWithIncrement (position, increment) {
+    return bigintCryptoUtils.modInv(increment, DECKSIZE) * BigInt(position) % BigInt(DECKSIZE);
 }        
 
-function cutNCards (cut) {
-    const index = (cut > 0 ? cut: DECKSIZE + cut);
-    if (lastPosition<index) {
-        lastPosition=DECKSIZE+lastPosition-index;
-    } else {
-        lastPosition=lastPosition-index;
-    }
+function reverseCutNCards (position, cut) {
+    return (BigInt(cut) + DECKSIZE + BigInt(position)) % DECKSIZE
 }        
 
-shared.start("day 22B");
-for (let i = 0; i<1; i++) {
-    shuffles.forEach((shuffle, index) => {
-        switch(shuffle.type) {
-            case 0: 
-                deck = cutNCards(shuffle.cut);
-                break;
-            case 1: 
-                deck = dealWithIncrement(shuffle.increment);
-                break;
-            case 2: 
-                deck = dealIntoNewStack();
-                break;
-        }    
-    });
+function applyShuffle (position, shuffle) {
+    switch(shuffle.type) {
+        case 0: 
+            return reverseCutNCards(position, shuffle.cut);
+        case 1: 
+            return reverseDealWithIncrement(position, shuffle.increment);
+        case 2: 
+            return reverseDealIntoNewStack(position);
+    }    
 }
 
-shared.end(lastPosition);
+shared.start("day 22B");
+let answer=0;
+const y = shuffles.reduce(applyShuffle, SEARCHPOSITION);
+const z = shuffles.reduce(applyShuffle, y);
+const a = (y - z) * bigintCryptoUtils.modInv(SEARCHPOSITION - y + DECKSIZE, DECKSIZE) % DECKSIZE
+const b = (y - a * SEARCHPOSITION) % DECKSIZE;
+
+answer = (bigintCryptoUtils.modPow(a, SHUFFLES, DECKSIZE) * SEARCHPOSITION + (bigintCryptoUtils.modPow(a, SHUFFLES, DECKSIZE) - BigInt(1)) * bigintCryptoUtils.modInv(a - BigInt(1), DECKSIZE) * b) % DECKSIZE;
+
+shared.end(answer);
 
 
